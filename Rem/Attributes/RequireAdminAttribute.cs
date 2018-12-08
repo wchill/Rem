453de9d6765936f8a0a0c441b8b4e4506e9e2f88
Dat Chive <gerr0041@umn.edu>
2018-12-07 17:03:24 -0800

    chore(attribute-cleanup): removing unused imports, cleaning logic

--- Rem/Attributes/RequireAdminAttribute.cs
+++ Rem/Attributes/RequireAdminAttribute.cs
@@ -8,7 +8,7 @@ namespace Rem.Attributes
 {
     public class RequireAdminAttribute : PreconditionAttribute
     {
-        private const string ErrorLackPermissions = "You do not have permission to use this command.";
+        private const string ErrorLackPermission = "You do not have permission to use this command.";
 
         public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context,
             CommandInfo command, IServiceProvider services)
@@ -19,7 +19,7 @@ namespace Rem.Attributes
 
                 return context.User.Id == application.Owner.Id ?
                     PreconditionResult.FromSuccess() :
-                    PreconditionResult.FromError(ErrorLackPermissions);
+                    PreconditionResult.FromError(ErrorLackPermission);
             }
 
             var userId = context.User.Id;
@@ -27,7 +27,7 @@ namespace Rem.Attributes
 
             return state.AdminList.Contains(userId) ?
                 PreconditionResult.FromSuccess() :
-                PreconditionResult.FromError(ErrorLackPermissions);
+                PreconditionResult.FromError(ErrorLackPermission);
         }
     }
 }
