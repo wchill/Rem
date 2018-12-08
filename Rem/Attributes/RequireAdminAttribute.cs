692f5e407e0c0b2790eef508adca622d59692af7
Dat Chive <gerr0041@umn.edu>
2018-12-07 16:56:50 -0800

    chore(attribute-cleanup): removing unused imports, cleaning logic

--- Rem/Attributes/RequireAdminAttribute.cs
+++ Rem/Attributes/RequireAdminAttribute.cs
@@ -1,35 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using Discord.WebSocket;
 using Rem.Bot;
 
 namespace Rem.Attributes
 {
     public class RequireAdminAttribute : PreconditionAttribute
     {
+        private const string ErrorLackPermissions = "You do not have permission to use this command.";
 
-        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context,
+            CommandInfo command, IServiceProvider services)
         {
             if (context.Client.TokenType == TokenType.Bot)
             {
                 var application = await context.Client.GetApplicationInfoAsync();
-                if (context.User.Id == application.Owner.Id)
-                    return PreconditionResult.FromSuccess();
+
+                return context.User.Id == application.Owner.Id ?
+                    PreconditionResult.FromSuccess() :
+                    PreconditionResult.FromError(ErrorLackPermissions);
             }
 
             var userId = context.User.Id;
             var state = (BotState) services.GetService(typeof(BotState));
 
-            if (state.AdminList.Contains(userId))
-            {
-                return PreconditionResult.FromSuccess();
-            }
-
-            return PreconditionResult.FromError("You do not have permission to use this command.");
+            return state.AdminList.Contains(userId) ?
+                PreconditionResult.FromSuccess() :
+                PreconditionResult.FromError(ErrorLackPermissions);
         }
     }
 }
--- Rem/Attributes/RequireRoleAttribute.cs
+++ Rem/Attributes/RequireRoleAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -10,27 +10,25 @@ namespace Rem.Attributes
     public class RequireRoleAttribute : PreconditionAttribute
     {
         private readonly HashSet<string> _roles;
+
         public RequireRoleAttribute(params string[] roles)
         {
             _roles = new HashSet<string>();
+
             foreach (var role in roles)
             {
                 _roles.Add(role.ToLower());
             }
         }
 
-        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command,
+            IServiceProvider services)
         {
             var roles = ((SocketGuildUser) context.User).Roles;
-            foreach (var role in roles)
-            {
-                if (_roles.Contains(role.Name.ToLower()))
-                {
-                    return Task.FromResult(PreconditionResult.FromSuccess());
-                }
-            }
-
-            return Task.FromResult(PreconditionResult.FromError("You do not have permission to use this command."));
+            //does this really just need any match?
+            return Task.FromResult(roles.Any(role => _roles.Contains(role.Name.ToLower())) ?
+                PreconditionResult.FromSuccess() :
+                PreconditionResult.FromError("You do not have permission to use this command."));
         }
     }
 }
