16d52fff1d68431910bead8170526a560caebf54
Eric Ahn <wchill1337@gmail.com>
2019-07-03 23:32:01 -0700

    Don't redirect to https since the reverse proxy will take care of it

--- Rem/WebApi/Startup.cs
+++ Rem/WebApi/Startup.cs
@@ -48,13 +48,7 @@ namespace Rem.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
+            
             app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rem Web API"); });
